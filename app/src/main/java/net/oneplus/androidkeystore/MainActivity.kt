e11da58efeebd9a0a787b4189d64131dd9c8ce65
Martin Wang <martin.wang@oneplus.tw>
2018-04-26 15:50:19 +0800

    Mark user authentication

--- app/src/main/java/net/oneplus/androidkeystore/MainActivity.kt
+++ app/src/main/java/net/oneplus/androidkeystore/MainActivity.kt
@@ -7,12 +7,10 @@ import android.os.CancellationSignal
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -49,25 +47,25 @@ class MainActivity : AppCompatActivity() {
             if (iv.isEmpty()) {
                 Toast.makeText(this, "Please encrypt first", Toast.LENGTH_SHORT).show()
             } else {
-                text.text = dec(text.text.toString().hexToByteArray(), iv)
+                dec(text.text.toString().hexToByteArray(), iv)
             }
         }
 
         fingerprintManager = getSystemService(Context.FINGERPRINT_SERVICE) as FingerprintManager
     }
 
-    private fun checkFingerprint(cipher: Cipher, callback: FingerprintManager.AuthenticationCallback) {
-        if (!fingerprintManager!!.isHardwareDetected) {
-            Toast.makeText(this, "Fingerprint HW not detected", Toast.LENGTH_SHORT).show()
-            return
-        }
-        if (!fingerprintManager!!.hasEnrolledFingerprints()) {
-            Toast.makeText(this, "No fingerprint enrolled", Toast.LENGTH_SHORT).show()
-            return
-        }
-        cancellationSignal = CancellationSignal()
-        fingerprintManager!!.authenticate(FingerprintManager.CryptoObject(cipher), cancellationSignal, 0, callback, null)
-    }
+//    private fun checkFingerprint(cipher: Cipher, callback: FingerprintManager.AuthenticationCallback) {
+//        if (!fingerprintManager!!.isHardwareDetected) {
+//            Toast.makeText(this, "Fingerprint HW not detected", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+//        if (!fingerprintManager!!.hasEnrolledFingerprints()) {
+//            Toast.makeText(this, "No fingerprint enrolled", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+//        cancellationSignal = CancellationSignal()
+//        fingerprintManager!!.authenticate(FingerprintManager.CryptoObject(cipher), cancellationSignal, 0, callback, null)
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -86,15 +84,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun enc(textToEncrypt: String) {
-        val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE)
+//        if (!fingerprintManager!!.isHardwareDetected) {
+//            Toast.makeText(this, "Fingerprint HW not detected", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+//        if (!fingerprintManager!!.hasEnrolledFingerprints()) {
+//            Toast.makeText(this, "No fingerprint enrolled", Toast.LENGTH_SHORT).show()
+//            return
+//        }
 
         val keyGenParameterSpec = KeyGenParameterSpec.Builder(KEY_ALIAS,
                 KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
                 .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
                 .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                .setUserAuthenticationRequired(true)
+//                .setUserAuthenticationRequired(true)
                 .build()
 
+        val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE)
         keyGenerator.init(keyGenParameterSpec)
         val secretKey = keyGenerator.generateKey()
 
@@ -102,26 +108,30 @@ class MainActivity : AppCompatActivity() {
         cipher.init(Cipher.ENCRYPT_MODE, secretKey)
         iv = cipher.iv
 
-//        text.text = cipher.doFinal(textToEncrypt.toByteArray()).toHex()
+        val cipherText = cipher.doFinal(textToEncrypt.toByteArray()).toHex()
+        Log.d("Martin", "iv: ${iv.toHex()}, cipherText: $cipherText")
 
-        val dialog = AlertDialog.Builder(this).create()
-        dialog.setView(View.inflate(this, R.layout.fingerprint_dialog_content, null))
-        dialog.show()
+//        val dialog = AlertDialog.Builder(this).create()
+//        dialog.setView(View.inflate(this, R.layout.fingerprint_dialog_content, null))
+//        dialog.show()
+//
+//        checkFingerprint(cipher, object : FingerprintManager.AuthenticationCallback() {
+//            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+//                Log.i("Martin", "onAuthenticationError: err: $errString")
+//            }
+//
+//            override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
+//                Log.i("Martin", "onAuthenticationSucceeded")
+//                dialog.dismiss()
+//                text.text = cipher.doFinal(textToEncrypt.toByteArray()).toHex()
+//            }
+//
+//            override fun onAuthenticationFailed() {
+//                Log.i("Martin", "onAuthenticationFailed")
+//            }
+//        })
 
-        checkFingerprint(cipher, object : FingerprintManager.AuthenticationCallback() {
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
-                Log.i("Martin", "onAuthenticationError: err: $errString")
-            }
-
-            override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
-                Log.i("Martin", "onAuthenticationSucceeded")
-                text.text = cipher.doFinal(textToEncrypt.toByteArray()).toHex()
-            }
-
-            override fun onAuthenticationFailed() {
-                Log.i("Martin", "onAuthenticationFailed")
-            }
-        })
+        text.text = cipherText
 
 //        val factory = SecretKeyFactory.getInstance(secretKey.algorithm, "AndroidKeyStore")
 //        val keyInfo: KeyInfo
@@ -133,15 +143,44 @@ class MainActivity : AppCompatActivity() {
 //        }
     }
 
-    private fun dec(encryptedData: ByteArray, encryptedIv: ByteArray): String {
+    private fun dec(encryptedData: ByteArray, encryptedIv: ByteArray) {
+//        if (!fingerprintManager!!.isHardwareDetected) {
+//            Toast.makeText(this, "Fingerprint HW not detected", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+//        if (!fingerprintManager!!.hasEnrolledFingerprints()) {
+//            Toast.makeText(this, "No fingerprint enrolled", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+
         val keyStore = KeyStore.getInstance("AndroidKeyStore")
         keyStore.load(null)
         val secretKeyEntry = keyStore.getEntry(KEY_ALIAS, null) as KeyStore.SecretKeyEntry
         val secretKey = secretKeyEntry.secretKey
         val cipher = Cipher.getInstance("AES/GCM/NoPadding")
         val spec = GCMParameterSpec(128, encryptedIv)
-
         cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
-        return String(cipher.doFinal(encryptedData))
+
+//        val dialog = AlertDialog.Builder(this).create()
+//        dialog.setView(View.inflate(this, R.layout.fingerprint_dialog_content, null))
+//        dialog.show()
+//
+//        checkFingerprint(cipher, object : FingerprintManager.AuthenticationCallback() {
+//            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+//                Log.i("Martin", "onAuthenticationError: err: $errString")
+//            }
+//
+//            override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
+//                Log.i("Martin", "onAuthenticationSucceeded")
+//                dialog.dismiss()
+//                text.text = String(cipher.doFinal(encryptedData))
+//            }
+//
+//            override fun onAuthenticationFailed() {
+//                Log.i("Martin", "onAuthenticationFailed")
+//            }
+//        })
+
+        text.text = String(cipher.doFinal(encryptedData))
     }
 }
